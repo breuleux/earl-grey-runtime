f9ee42f68d55b8091e524e90181ab2bc5dca5b61
Olivier Breuleux <breuleux@gmail.com>
2015-02-21 23:41:51 -0500

    babel-runtime does not work well for my purposes

--- 5.js
+++ 5.js
@@ -1,7 +1,7 @@
 
 if (!global._egruntime_installed) {
     global._egruntime_installed = true;
-    require("babel-runtime/core-js");
-    regeneratorRuntime = require("babel-runtime/regenerator")["default"];
+    require("core-js/shim");
+    require("regenerator-babel/runtime");
     require("./lib");
 }
