d02c5f1f4e435423e511d79ca521c6575459620c
Olivier Breuleux <breuleux@gmail.com>
2015-04-22 20:34:24 -0400

    spin off dir function

--- lib.js
+++ lib.js
@@ -632,40 +632,6 @@ range.prototype["::contains"] = function (x) {
 global["range"] = range;
 
 
-function dir(arg) {
-    function helper(arg) {
-        if (typeof(arg) === "number")
-            return helper(Number.prototype);
-        else if (typeof(arg) === "string")
-            return helper(String.prototype);
-        else if (arg === true || arg === false)
-            return helper(Boolean.prototype);
-        else {
-            var curr = arg;
-            var results = [];
-            while (curr && (arg === Object || curr !== Object)) {
-                var props = Object.getOwnPropertyNames(curr);
-                for (var i = 0; i < props.length; i++) {
-                    var k = props[i];
-                    if (!k.match(/^toString|^__|^::/)) {
-                        try {
-                            results.push([k, arg[k]]);
-                        }
-                        catch (e) {
-                            results.push([k, e]);
-                        }
-                    }
-                }
-                curr = Object.getPrototypeOf(curr);
-            }
-            return object(results.sort());
-        }
-    }
-    return helper(arg);
-}
-global["dir"] = dir;
-
-
 function getChecker(type) {
     var f = type["::check"];
     if (f === undefined) {
