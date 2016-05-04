4423173b53e1ecb0b0739da74bfbec54f90f14e4
Tom X. Tobin <tomxtobin@tomxtobin.com>
2016-05-04 14:12:32 -0400

    Use regenerator-runtime package
    The regenerator package warns when requiring "regenerator/runtime" that
    the module is deprecated, and to use "regenerator-runtime/runtime"
    instead.

--- 5.js
+++ 5.js
@@ -2,11 +2,11 @@
 if (!global._egruntime_installed) {
     global._egruntime_installed = 5;
     require("core-js/shim");
-    require("regenerator/runtime");
+    require("regenerator-runtime/runtime");
     require("./lib");
 }
 else if (global._egruntime_installed == 6) {
     global._egruntime_installed = 5;
     require("core-js/shim");
-    require("regenerator/runtime");
+    require("regenerator-runtime/runtime");
 }
